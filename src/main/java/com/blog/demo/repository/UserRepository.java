0ce6714caf083e41cb25c4c6d3d99917921fade9
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-12-30 11:45:19 +0900

    db에 데이터 넣거나 할 때 사용할 repository 생성

---
+++ src/main/java/com/blog/demo/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.blog.demo.repository;
+
+import com.blog.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// 자동으로 bean으로 등록된다 = 어디서나 injection 가능(DI)
+
+// 아래 제네릭의 의미 <User, Integer>
+// "이 repository는 User 테이블을 관리하며, 이 user 테이블의 PK는 integer이다.
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // JpaRepository의 findAll()
+    // -> 이 테이블의 모든 행을 리턴
+
+
+
+}
