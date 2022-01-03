3ad79c9e0c04a08e2b1fea3f2269cc0b0a2c5c80
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-01-03 09:43:45 +0900

    delete 테스트

--- src/main/java/com/blog/demo/test/DummyControllerTest.java
+++ src/main/java/com/blog/demo/test/DummyControllerTest.java
@@ -4,15 +4,14 @@ import com.blog.demo.model.RoleType;
 import com.blog.demo.model.User;
 import com.blog.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -22,6 +21,44 @@ public class DummyControllerTest {
 
     @Autowired // 의존성 주입(DI)
     private UserRepository userRepository;
+
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable Long id) {
+        try {
+            // 없는 id로 지우려고 할 수도 있으니까 exception을 잡기 위해서
+            // try catch로 감싼다.
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return "삭제에 실패하였습니다.";
+        }
+        return "삭제되었음";
+    }
+
+    @Transactional // 함수 종료시에 자동으로 commmit 됨
+    @PutMapping("/dummy/user/{id}")
+    // Json으로 받으려면 @RequestBody 어노테이션 필요
+    // json 데이터를 MessageConverter가 자바 오브젝트로 바꿔줌
+    public User updateUser(@PathVariable Long id, @RequestBody User requestUser) {
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+        requestUser.setId(id);
+        // JPA의 save 함수를 업데이트 할 때도 사용하려면 아래처럼 해야함
+        // 1. id로 user를 찾고
+        // 2. json으로 받은 password, email을 찾은 user에 업데이트
+        // 3. 이 user로 save
+        // 그래야 여기서 받은 email, password 외의 값에 null이 들어가지 않음
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("수정에 실패하였습니다");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+        // userRepository.save(user);
+
+        // 그런데 save 함수 말고 다른 방법은... @Transactional 사용임. (더티체킹)
+        // @Transactional 을 사용하면 .save 함수를 사용하지 않아도 업데이트 된다.
+        return user;
+    }
 
     @GetMapping("/dummy/users")
     public List<User> list() {
