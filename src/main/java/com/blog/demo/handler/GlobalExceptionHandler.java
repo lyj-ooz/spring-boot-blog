401f2cbebec843d2f8f8b0add5d741568f4d370d
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-01-07 17:03:23 +0900

    responseDto 적용

--- src/main/java/com/blog/demo/handler/GlobalExceptionHandler.java
+++ src/main/java/com/blog/demo/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.blog.demo.handler;
 
+import com.blog.demo.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,14 @@ public class GlobalExceptionHandler {
 
     // IllegalArgumentException 이 발생하면 그 exception을
     // 아래 함수에 전달함
-    @ExceptionHandler(value = IllegalArgumentException.class)
-    public String handleArgumentException(IllegalArgumentException e) {
-        return "<h1>" + e.getMessage() + "</h1>";
-    }
-
-    // 또는 아래처럼 모든 exception을 다 잡도록 작성할 수도 있음
-//    @ExceptionHandler(value = Exception.class)
-//    public String handleArgumentException(Exception e) {
+//    @ExceptionHandler(value = IllegalArgumentException.class)
+//    public String handleArgumentException(IllegalArgumentException e) {
 //        return "<h1>" + e.getMessage() + "</h1>";
 //    }
+
+    // 또는 아래처럼 모든 exception을 다 잡도록 작성할 수도 있음
+    @ExceptionHandler(value =  Exception.class)
+    public ResponseDto<String> handleArgumentException(Exception e) {
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
 }
