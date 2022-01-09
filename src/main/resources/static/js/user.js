7a75dd389bd002d626805ff6af2f00e9f49f3e8b
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-01-09 14:10:25 +0900

    뷰 - 로그인 추가

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,10 @@ let index = {
         $("#btn-save").on("click", () => {
             this.save();
         })
+
+        $("#btn-login").on("click", () => {
+            this.login();
+        })
     },
 
     save: function() {
@@ -28,7 +32,31 @@ let index = {
         }).fail(function(err) {
             alert(JSON.stringify(err));
         });
-    }
+    },
+
+    login: function() {
+            let data = {
+                username: $("#username").val(),
+                password: $("#pwd").val(),
+            };
+
+            console.log(data);
+
+            // ajax 호출시 default가 비동기 호출
+            $.ajax({
+                type: "POST", // GET 방식은 주소에 다 노출되니까 위험
+                url: "/blog/api/user/login",
+                data: JSON.stringify(data), // http body 데이터
+                contentType: "application/json; charset=utf-8", // body데이터 타입
+                dataType: "json" // 응답 타입
+            }).done(function(response){
+                console.log(response);
+                alert("로그인 완료!");
+                location.href = "/blog"
+            }).fail(function(err) {
+                alert(JSON.stringify(err));
+            });
+        }
 }
 
 index.init();
