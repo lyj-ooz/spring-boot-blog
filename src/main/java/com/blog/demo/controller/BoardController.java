3a164f6d6a62ce4fc1f2281e1d250bc84512d3dd
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-01-10 21:48:58 +0900

    컨트롤러에서 로그인 유저 세션 얻는 부분 추가

--- src/main/java/com/blog/demo/controller/BoardController.java
+++ src/main/java/com/blog/demo/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.blog.demo.controller;
 
+import com.blog.demo.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping({"","/"}) // 아무것도 안 적었을때 & '/' 붙였을때 !!!
-    public String index() {
+    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
+        System.out.println("로그인 사용자 아이디 " + principal.getUsername());
+
         // /WEB-INF/views/index.jsp
         return "index";
+
+        // 이 주소는 로그인 후 오는 주소이기도 함
+        // 그러면 컨트롤러에서 세션을 어떻게 찾나? -> @AuthenticationPrincipal
+
+
     }
 }
